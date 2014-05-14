757e8bbfcd7c33bbf04c0fd241065838447a8820
Marc Meszaros <marc@marcmeszaros.ca>
2014-05-14 01:04:13 -0400

    Finally get 'Hello World' working

--- server/app.js
+++ server/app.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+var path = require('path');
+
+// Database
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/5by-challenge", {native_parser:true});
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+// routes to use
+app.use('/', routes);
+
+module.exports = app;
